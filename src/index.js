8acd5b8d92550106b26733a47ab44539c5a8323d
oe0315 <oeoe0315@gmail.com>
2020-07-03 00:52:58 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
+
+const TodoApp = () => {
+  const [value, setValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleChange = e => {
+    const newValue = e.target.value;
+    setValue(newValue);
+  };
+
+  const add = () => {
+    const newTodo = [...todoList, value];
+    setTodoList(newTodo);
+
+    setValue("")
+  };
+
+  return (
+    <div>
+      <h1>TODO App</h1>
+      <div>
+        <input type="text" value={value} onChange={handleChange} />
+        <ul>
+          {todoList.map((todo, i) => (
+            <li key={i}>{todo}</li>
+          ))}
+        </ul>
+      </div>
+      <button onClick={add}>追加</button>
+    </div>
+  );
+};
+
+
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('root')
+);
